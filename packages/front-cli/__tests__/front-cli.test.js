e0e5e61bdd24a7c76edb80df9973a8e8b15eb7c9
arale-dev <9801gksquf@gmail.com>
2022-01-20 17:49:07 +0900

    [feat] front cli settings

--- packages/front-cli/__tests__/front-cli.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const frontCli = require('..');
-
-describe('front-cli', () => {
-    it('needs tests');
-});
--- packages/front-cli/lib/front-cli.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = frontCli;
-
-function frontCli() {
-    // TODO
-}
